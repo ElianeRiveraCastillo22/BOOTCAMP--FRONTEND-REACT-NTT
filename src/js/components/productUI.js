2fe70e32cde9b9e2260d4b37599d6746515e375e
ElianeRiveraCastillo22 <elianercast@gmail.com>
2024-11-14 14:38:01 -0500

    feat: [ Renderizar productos en el home ]

--- src/js/components/productUI.js
+++ src/js/components/productUI.js
@@ -1,8 +1,19 @@
 import { BASE_API, getAllProducts } from "../services/api.js"
+import { templateProductCard } from "../template/productCard.js"
+const productsList = document.querySelector(".products__list")
+
+function showProduct(arrProducts) {
+
+    arrProducts.products.forEach(product => {
+        productsList.appendChild(templateProductCard(product.images[0], product.availabilityStatus, product.price, product.title))
+    });
+
+}
 
 async function renderProducts() {
 
     const arrayCategoryList = await getAllProducts(BASE_API, '/products?skip=120')
+    showProduct(arrayCategoryList)
 
 }
 export {
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 import { renderCategories } from "./components/categoryUI.js";
+import { renderProducts } from "./components/productUI.js";
 
 document.addEventListener("DOMContentLoaded", async()=>{
 
     await renderCategories()
+    await renderProducts()
 
 })
