7d2c1b9ed42ebceb0325f3c8ac9f0f19dbe284a8
ElianeRiveraCastillo22 <elianercast@gmail.com>
2024-11-13 20:30:22 -0500

    feat: [ Renderizar lista de categorías en HTML ]

--- src/js/components/categoryUI.js
+++ src/js/components/categoryUI.js
@@ -1,11 +1,19 @@
 import { getProductCategoryList, BASE_API } from "../services/api.js";
+import { templateCategoryCart } from "../template/categoryCart.js";
+
+const carouselTrack = document.querySelector(".carousel-track")
 
 async function showCategoryList() {
 
     const arrayCategoryList = await getProductCategoryList(BASE_API, '/products/category-list')
+    arrayCategoryList.forEach(category => {
+        carouselTrack.appendChild(templateCategoryCart(`assets/icons/category/${category}.svg`, category))
+    });
 
 }
 
 export async function renderCategories() {
+
     await showCategoryList()
+
 }
---
+++ src/js/template/categoryCart.js
@@ -0,0 +1,18 @@
+export function templateCategoryCart(imgCategory, nameCategory ) {
+
+    const cardCategory = document.createElement("figure")
+    cardCategory.classList.add("categories__item")
+
+    const elementImg = document.createElement("img")
+    elementImg.src = imgCategory
+    elementImg.alt = nameCategory
+
+    const elementDescription = document.createElement("figcaption")
+    elementDescription.textContent = nameCategory
+
+    cardCategory.appendChild(elementImg)
+    cardCategory.appendChild(elementDescription)
+
+    return cardCategory
+
+}
