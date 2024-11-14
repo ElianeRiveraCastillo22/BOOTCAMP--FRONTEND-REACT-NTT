63856d431aa7b2b33ff2be5d35d70933e1a9688b
ElianeRiveraCastillo22 <elianercast@gmail.com>
2024-11-13 20:43:04 -0500

    fix: [ Actualizar control del carrusel para sincronizar con lista de categorías ]

--- src/js/components/carouselUI.js
+++ src/js/components/carouselUI.js
@@ -9,9 +9,11 @@ const control = document.querySelector(".categorie__control")
 
 const carouselSize = carousel.clientWidth
 
-function createCarouselPoints() {
-    if(calculateNumberOfCarouselPoints() !== 1){
-        for (let index = 0; index < calculateNumberOfCarouselPoints() ; index++) {
+function createCarouselPoints(sizeOfCategoryCards, numberOfCardsByCategory) {
+
+    if(calculateNumberOfCarouselPoints(sizeOfCategoryCards, numberOfCardsByCategory) !== 1){
+
+        for (let index = 0; index < calculateNumberOfCarouselPoints(sizeOfCategoryCards, numberOfCardsByCategory) ; index++) {
             const small = document.createElement("small")
             small.classList.add("control__item")
             control.append(small)
@@ -19,32 +21,38 @@ function createCarouselPoints() {
         const firtDot = document.querySelectorAll(".categorie__control small")[0]
         firtDot.classList.add("selectedPoint")
     }
+
 }
 
 function addStylesToTheDot(dotsControl, dot) {
+
     if(!dot.classList.contains("selectedPoint")){
         const selectedPoint= Object.values(dotsControl).filter((dots)=>{return dots.classList.contains("selectedPoint")})[0]
         dot.classList.add("selectedPoint")
         selectedPoint.classList.remove("selectedPoint")
     }
+
 }
 
-function moveCarousel(dotsControl, dotIndex) {
-    if(dotsControl.length-1 == dotIndex) carruselTrack.style.transform = `translateX(-${calculateLastSectionShiftSize()}px)`
+function moveCarousel(dotsControl, dotIndex, sizeOfCategoryCards, numberOfCardsByCategory) {
+
+    if(dotsControl.length-1 == dotIndex) carruselTrack.style.transform = `translateX(-${calculateLastSectionShiftSize(sizeOfCategoryCards, numberOfCardsByCategory)}px)`
     else carruselTrack.style.transform = `translateX(-${dotIndex *carouselSize}px)`
+
 }
 
-function initializeCarousel() {
-    createCarouselPoints()
+function initializeCarousel(numberOfCardsByCategory,sizeOfCategoryCards ) {
 
+    createCarouselPoints(sizeOfCategoryCards, numberOfCardsByCategory)
     const dotsControl = document.querySelectorAll(".categorie__control small")
 
     function addControlToPoints(dot, dotIndex) {
         dot.addEventListener("click",()=>{addStylesToTheDot(dotsControl, dot)} )
-        dot.addEventListener("click",()=>{moveCarousel(dotsControl, dotIndex)} )
+        dot.addEventListener("click",()=>{moveCarousel(dotsControl, dotIndex, sizeOfCategoryCards, numberOfCardsByCategory)} )
     }
 
     dotsControl.forEach(addControlToPoints)
+
 }
 
 export{
--- src/js/modules/carousel.js
+++ src/js/modules/carousel.js
@@ -1,24 +1,22 @@
-const carousel = document.querySelector(".carousel-track")
 const carruselTrack = document.querySelector(".carousel-track")
-const categoriesItem = document.querySelector(".categories__item")
-const categoriesItems = document.querySelectorAll(".categories__item")
-
 const categoryCardGap = 16
-const carouselSize = carousel.clientWidth
-const sizeOfCategoryCards = categoriesItem.clientWidth+categoryCardGap
-const numberOfCardsByCategory = categoriesItems.length
+const carouselSize = carruselTrack.clientWidth
 
-function calculateNumberOfCarouselPoints() {
-    const cardsThatFitTheScreen = carruselTrack.clientWidth / (categoriesItem.clientWidth+categoryCardGap)
+function calculateNumberOfCarouselPoints(sizeOfCategoryCards, numberOfCardsByCategory) {
+
+    const cardsThatFitTheScreen = carruselTrack.clientWidth / (sizeOfCategoryCards)
     const numberOfCarouselPoints = Math.ceil(numberOfCardsByCategory/cardsThatFitTheScreen)
     return numberOfCarouselPoints
+
 }
-function calculateLastSectionShiftSize() {
-    const antepenultimateSectionCardsWidth = (calculateNumberOfCarouselPoints()-2)*carouselSize;
+function calculateLastSectionShiftSize(sizeOfCategoryCards, numberOfCardsByCategory) {
+
+    const antepenultimateSectionCardsWidth = (calculateNumberOfCarouselPoints(sizeOfCategoryCards, numberOfCardsByCategory)-2)*carouselSize;
     const trackSize = numberOfCardsByCategory*sizeOfCategoryCards
-    const trackused = (calculateNumberOfCarouselPoints()-1)*(carouselSize)
+    const trackused = (calculateNumberOfCarouselPoints(sizeOfCategoryCards, numberOfCardsByCategory)-1)*(carouselSize)
     const lastSectionCardsWidth = trackSize - trackused
     return (antepenultimateSectionCardsWidth+ lastSectionCardsWidth)-categoryCardGap
+
 }
 
 export {
