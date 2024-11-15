1908706e361c6ba139da96be768b12c646ccd892
ElianeRiveraCastillo22 <elianercast@gmail.com>
2024-11-15 01:53:00 -0500

    chore: [ Eliminar comentarios del template de carta de productos ]

--- src/js/template/productCard.js
+++ src/js/template/productCard.js
@@ -2,47 +2,42 @@ export function templateProductCard(imgurlProduct, descriptionProduct, priceProd
     const product = document.createElement('article');
     product.classList.add('products__item');
 
-    // Contenedor de la imagen
     const productImageBox = document.createElement('figure');
     productImageBox.classList.add('products__image');
 
     const productImage = document.createElement('img');
     productImage.src = imgurlProduct;
-    productImage.alt = 'Producto Blossom Pouch'; // Agregar texto alternativo
+    productImage.alt = 'Producto Blossom Pouch';
+
     productImageBox.appendChild(productImage);
 
-    // Contenedor de la descripción
     const descriptionContainer = document.createElement('section');
     descriptionContainer.classList.add('products__description');
 
-    // Título del producto
     const title = document.createElement('p');
-    title.classList.add('products__title'); // Cambiar a 'products__title'
+    title.classList.add('products__title');
     title.textContent = nameProduct;
     descriptionContainer.appendChild(title);
 
-    // Descripción del producto
     const description = document.createElement('p');
-    description.classList.add('products__descriptionText'); // Cambiar para evitar duplicado
+    description.classList.add('products__descriptionText');
     description.textContent = descriptionProduct;
     descriptionContainer.appendChild(description);
 
-    // Precio del producto
     const price = document.createElement('p');
     price.classList.add('products__price');
     price.textContent = priceProduct;
     descriptionContainer.appendChild(price);
 
-    // Botón de agregar al carrito
     const button = document.createElement('button');
     button.classList.add('products__add-to-cart');
 
     const containerSvg = document.createElement('figure');
-    containerSvg.classList.add('products__cart'); 
+    containerSvg.classList.add('products__cart');
 
     const iconBag = document.createElement("img")
     iconBag.src = "assets/icons/bag.svg";
-    iconBag.alt = 'icon bag';  
+    iconBag.alt = 'icon bag';
 
     containerSvg.appendChild(iconBag)
 
@@ -57,15 +52,12 @@ export function templateProductCard(imgurlProduct, descriptionProduct, priceProd
     svg.appendChild(use);
     button.appendChild(svg); */
 
-    // Texto del botón
     const buttonText = document.createElement('p');
     buttonText.textContent = 'Agregar al carrito';
 
-    // Armar el botón
     button.appendChild(containerSvg)
     button.appendChild(buttonText);
 
-    // Armar el artículo
     product.appendChild(productImageBox);
     product.appendChild(descriptionContainer);
     product.appendChild(button);
