5c55cb5a3d9a34e9606933ec23162ee81d1c9aa8
ElianeRiveraCastillo22 <elianercast@gmail.com>
2024-12-01 20:11:30 -0500

    feat: [ agregar página para rutas no encontradas ]

--- src/pages/components/PageNotFound/PageNotFound.tsx
+++ src/pages/components/PageNotFound/PageNotFound.tsx
@@ -1,9 +1,14 @@
+import "./PageNotFound.css";
 export function PageNotFound() {
 
     return (
-      <p>
-        PageNotFound
-      </p>
-    )
-
+        <section className="error-page">
+            <h1 className="error-page__code">404</h1>
+            <h2 className="error-page__title">Oops! Page Not Found</h2>
+            <p className="error-page__description">
+                Sorry, the page you're looking for doesn't exist or has been
+                moved.
+            </p>
+        </section>
+    );
 }
