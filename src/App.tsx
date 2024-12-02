b8741420972e708b5c33dce61294d86138054381
ElianeRiveraCastillo22 <elianercast@gmail.com>
2024-12-02 18:21:59 -0500

    feat: [ agregar ErrorBoundary ]

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import { AppRouter } from "./AppRouter";
 import { FilterProvider } from "./context/filters/filters.provider";
 import { ShoppingCartProvider } from "./context/ShoppingCart/ShoppingCart.provider";
 import { ModalProvider } from "./context/Modal/ModalContext";
+import { ErrorBoundary } from "./ErrorBoundary";
 
 export function App() {
     return (
-        <ModalProvider>
-            <ShoppingCartProvider>
-                <FilterProvider>
-                    <AppRouter />
-                </FilterProvider>
-            </ShoppingCartProvider>
-        </ModalProvider>
+        <ErrorBoundary>
+            <ModalProvider>
+                <ShoppingCartProvider>
+                    <FilterProvider>
+                        <AppRouter />
+                    </FilterProvider>
+                </ShoppingCartProvider>
+            </ModalProvider>
+        </ErrorBoundary>
     );
 }
