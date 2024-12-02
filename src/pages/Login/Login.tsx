cdea795ec0d17c5c8ad28650a3b166585109b20e
ElianeRiveraCastillo22 <elianercast@gmail.com>
2024-12-01 20:14:34 -0500

    feat: [ agregar página de login ]

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,42 @@
+import "css/form.css";
+import { InputForm } from "../components/Form/InputForm";
+import { ButtonForm } from "../components/Form/ButtonForm";
+export const Login = () => {
+    return (
+        <div className="form-container">
+            <section className="form-section">
+                <figure className="form-logo">
+                    <img
+                        src="src/assets/logos/logo-shop-main.svg"
+                        alt="Logo de la tienda"
+                    />
+                </figure>
+                <form className="form">
+                    <InputForm
+                        type="email"
+                        id="Email"
+                        placeholder="Email"
+                        name="Email"
+                    />
+                    <InputForm
+                            type="password"
+                            id="Password"
+                            placeholder="Password"
+                            name="Password"
+                    />
+                    <ButtonForm nameButton="Log in"/>
+                    <footer className="form-footer">
+                        <button
+                            type="button"
+                            className="form-button form-button--forgot"
+                        >
+                            Forgot Password
+                        </button>
+                        <a href="/signup" className="form-link">Sign up</a>
+                    </footer>
+                </form>
+            </section>
+        </div>
+
+    );
+};
