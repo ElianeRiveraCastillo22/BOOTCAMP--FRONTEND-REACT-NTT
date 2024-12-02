d9ced458c3b897a889bbb3a6dd47139e07b8107d
ElianeRiveraCastillo22 <elianercast@gmail.com>
2024-12-02 18:34:27 -0500

    feat: [ agregar popup del componente ForgotPassword con validaciones ]

--- src/pages/Login/components/ForgotPassword.tsx
+++ src/pages/Login/components/ForgotPassword.tsx
@@ -1,30 +1,66 @@
-import { ButtonForm } from "@/pages/components/Form/ButtonForm"
-import { InputForm } from "@/pages/components/Form/InputForm"
-import "./ForgotPassword.css"
-export const ForgotPassword = () =>{
-    return(
-        <section className="form-section form-section--forgotPassword">
-        <figure className="form-logo--forgotPassword">
-            <img
-                src="src/assets/logos/logo-shop-main.svg"
-                alt="Logo de la tienda"
-            />
-        </figure>
-        <form className="form">
-            <InputForm
-                type="email"
-                id="Email"
-                placeholder="Ingresa tu email"
-                name="Email"
-            />
-            <InputForm
-                    type="password"
-                    id="Password"
-                    placeholder="Password"
-                    name="Password"
-            />
-            <ButtonForm nameButton="Register new password"/>
-        </form>
-    </section>
-    )
-}
+import { ButtonForm } from "@/pages/components/Form/ButtonForm";
+import { InputForgotPassword } from "@/pages/components/Form/InputForm/InputForgotPassword";
+import { PopUpConfirm } from "@/pages/components/Form/PopUpConfirm/PopUpConfirm";
+import {
+    FormValuesForgotPassword,
+    schemaForgotPassword,
+} from "@/pages/components/Form/schema/form.schema";
+import { zodResolver } from "@hookform/resolvers/zod/src/zod.js";
+import { SubmitHandler, useForm } from "react-hook-form";
+import "./ForgotPassword.css";
+import { useState } from "react";
+import { useModalContext } from "@/context/Modal/UseModalContext";
+
+export const ForgotPassword = () => {
+
+    const { setIsOpenModal } = useModalContext();
+    const [openPopUp, setOpenPopUp] = useState(false);
+
+    const {
+        control,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<FormValuesForgotPassword>({
+        resolver: zodResolver(schemaForgotPassword),
+        mode: "onBlur",
+        defaultValues: {
+            email: "emily.johnson@x.dummyjson.com",
+        },
+    });
+
+    const onSubmit: SubmitHandler<FormValuesForgotPassword> = () => {
+        setOpenPopUp(!openPopUp);
+        setTimeout(() => {
+            setIsOpenModal(false);
+        }, 800);
+    };
+
+    return (
+        <>
+            <section className="form-section form-section--forgotPassword">
+                <figure className="form-logo--forgotPassword">
+                    <img
+                        src="src/assets/logos/logo-shop-main.svg"
+                        alt="Logo de la tienda"
+                    />
+                </figure>
+                <h1 className="form__forgotPassword--title">Reset password</h1>
+                <form className="form" onSubmit={handleSubmit(onSubmit)}>
+                    <InputForgotPassword
+                        name="email"
+                        control={control}
+                        type="email"
+                        error={errors.email}
+                        placeholder="Email"
+                    />
+                    <ButtonForm type="submit" nameButton="Send" />
+                </form>
+            </section>
+            {openPopUp && (
+                <PopUpConfirm
+                    message={"Se envió la información al correo ingresado."}
+                />
+            )}
+        </>
+    );
+};
