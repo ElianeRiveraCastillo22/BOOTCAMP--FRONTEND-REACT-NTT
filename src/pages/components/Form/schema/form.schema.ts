78259d979d4f6fce3a9f8603f239ff9531685cf2
ElianeRiveraCastillo22 <elianercast@gmail.com>
2024-12-02 18:25:00 -0500

    feat: [ agregar esquema de validación para los formularios ]

---
+++ src/pages/components/Form/schema/form.schema.ts
@@ -0,0 +1,24 @@
+import { z } from "zod"
+
+export const schemaLogin = z.object({
+    username: z.string().min(1, "The name is required"),
+    email: z.string().email("Invalid email").min(1, "Mail is required"),
+    password: z. string().min(5, "Invalid password: must be at least 6 characters."),
+})
+
+export const schemaSignUp = z.object({
+    username: z.string().min(1, "The name is required").regex(/^[^\d]*$/, "The name cannot contain numbers"),
+    email: z.string().email("Invalid email").min(1, "Mail is required"),
+    password: z. string().min(5, "Invalid password: must be at least 6 characters."),
+    confirmPassword: z.string().min(6, "Passwords do not match. Please check and try again")
+}).refine((data)=>data.password=== data.confirmPassword,{
+    message: "Passwords are invalid. Make sure they meet the requirements.",
+    path: ["confirmPassword"]
+})
+export const schemaForgotPassword = z.object({
+    email: z.string().email("Invalid email").min(1, "Mail is required"),
+})
+
+export type FormValuesLogin = z.infer<typeof schemaLogin>
+export type FormValuesSignUp = z.infer<typeof schemaSignUp>
+export type FormValuesForgotPassword = z.infer<typeof schemaForgotPassword>
