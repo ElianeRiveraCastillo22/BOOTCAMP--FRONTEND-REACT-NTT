81377151cc34a329f66f7b70eefe2775b1d4b9aa
ElianeRiveraCastillo22 <elianercast@gmail.com>
2024-12-02 23:48:03 -0500

    build: [ agregar configuraciones para CORS ]

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,16 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+    plugins: [react(), tsconfigPaths()],
+		server: {
+			proxy: {
+				"/api": {
+					target: "https://dummyjson.com",
+					changeOrigin: true,
+					secure: false,
+					rewrite: (p) => p.replace(/^\/api/, ""),
+				},
+			},
+			cors: false,
+		},
 });
